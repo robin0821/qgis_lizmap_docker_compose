649b59444d2616d841b76f39867fbb5cbff4f83a
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-03-26 10:14:38 +1100

    Update start.sh

--- qgis_server3/start.sh
+++ qgis_server3/start.sh
@@ -1,8 +1,13 @@
+#!/bin/sh
 set -e
 
 # Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
 rm -f /usr/local/apache2/logs/httpd.pid
-#xvfb
+#remove pre-existing xvfb setting file
+rm -f /tmp/.X99-lock
+rm -rf /tmp/.X11-unix
+#start xvfb process
 /usr/bin/Xvfb  :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
 
 service apache2 restart
