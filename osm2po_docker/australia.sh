08eb2778a7e1215dfc9987a8963970ed10dbe738
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-04-17 12:00:53 +1000

    Update australia.sh

--- osm2po_docker/australia.sh
+++ osm2po_docker/australia.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 java -Xmx2g -jar osm2po-core-5.2.43-signed.jar cmd=tjspg prefix=australia tileSize=x https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter postp.1.class = de.cm.osm2po.plugins.postp.PgVertexWriter
