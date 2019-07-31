bd0748d95e73452e1960ca6d983adf1eb596c553
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-07-31 10:19:47 +1000

    Add files via upload

---
+++ osm2po_venus/venus.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# java -Xmx1g -jar osm2po-core-5.2.43-signed.jar prefix=hh tileSize=x https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter
+
+java -Xmx8g -jar osm2po-core-5.2.43-signed.jar prefix=venus tileSize=x https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter postp.1.class = de.cm.osm2po.plugins.postp.PgVertexWriter
