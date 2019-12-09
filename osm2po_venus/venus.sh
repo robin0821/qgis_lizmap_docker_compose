168694702c3c337331154b65bc0cb29c3c9736c0
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-12-10 09:51:13 +1100

    Update venus.sh

--- osm2po_venus/venus.sh
+++ osm2po_venus/venus.sh
@@ -2,4 +2,4 @@
 
 # java -Xmx1g -jar osm2po-core-5.2.43-signed.jar prefix=hh tileSize=x https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter
 
-java -Xmx8g -jar osm2po-core-5.2.43-signed.jar prefix=venus tileSize=x https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf postp.0.class=de.cm.osm2po.plugins.postp.PgRoutingWriter postp.1.class = de.cm.osm2po.plugins.postp.PgVertexWriter
+java -Xmx8g -jar osm2po-core-5.2.43-signed.jar prefix=venus tileSize=x cmd=tjspg https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf
